3de95e16d418b9e6d493720b64ec3b3392fbbec9
khushiiakter <rkjia1311@gmail.com>
2024-09-23 20:31:06 +0600

    added a modal

--- js/index.js
+++ js/index.js
@@ -7,13 +7,20 @@
 // })
 
 document.getElementById('donate-btn').addEventListener('click', function(){
+    
     const inputAmount = getInputFieldValueById('input-amount');
     const currentDonation = getTextFieldById('current-donation');
     const accountBalance = getTextFieldById('account-balance');
     
 
+    if(inputAmount <= 0 || isNaN(inputAmount)){
+        alert("Invalid number.");
+        return;
+    }
+    
     const newCurrentDonation = inputAmount + currentDonation;
     const newAccountBalance = accountBalance - inputAmount;
+    
     
     document.getElementById('current-donation').innerText = newCurrentDonation;
     document.getElementById('account-balance').innerText = newAccountBalance;
