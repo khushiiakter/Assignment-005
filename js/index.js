ef6d0f4e1b7ff55905de23ac5752a5b69bf08c39
khushiiakter <rkjia1311@gmail.com>
2024-09-23 19:21:30 +0600

    added few js file and some functions

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// document.getElementById('donate-btn').addEventListener('click', function(){
+//     const inputAmount = parseFloat(document.getElementById('input-amount').value);
+//     const currentDonation = parseFloat(document.getElementById('current-donation').innerText);
+//     const newCurrentDonation = inputAmount + currentDonation;
+//     // console.log(currentDonation, inputAmount);
+//     document.getElementById('current-donation').innerText = newCurrentDonation;
+// })
+
+document.getElementById('donate-btn').addEventListener('click', function(){
+    const inputAmount = getInputFieldValueById('input-amount');
+    const currentDonation = getTextFieldById('current-donation');
+    const accountBalance = getTextFieldById('account-balance');
+    
+
+    const newCurrentDonation = inputAmount + currentDonation;
+    const newAccountBalance = accountBalance - inputAmount;
+    
+    document.getElementById('current-donation').innerText = newCurrentDonation;
+    document.getElementById('account-balance').innerText = newAccountBalance;
+
+})
+
+
---
+++ js/utilities.js
@@ -0,0 +1,8 @@
+function getInputFieldValueById(id){
+    const inputValue = parseFloat(document.getElementById(id).value);
+    return inputValue;
+}
+function getTextFieldById(id){
+    const textValue = parseFloat(document.getElementById(id).innerText);
+    return textValue;
+}
