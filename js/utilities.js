f8166a1c1d32d495d7d83dc64c948fab5f422c66
khushiiakter <rkjia1311@gmail.com>
2024-09-24 23:58:15 +0600

    added some changes in navbar

--- js/utilities.js
+++ js/utilities.js
@@ -7,3 +7,13 @@ function getTextFieldById(id){
     const textValue = parseFloat(document.getElementById(id).innerText);
     return textValue;
 }
+window.addEventListener('scroll', function(){ 
+    const navbar = document.getElementById('navbar');  
+
+    if(window.scrollY > 50){
+        // headerPart.classList.add("bg-white/50", "scrolled");
+        navbar.classList.remove('bg-[#F9F7F3]', 'scrolled');
+    }else{
+        navbar.classList.add("backdrop-blur", "bg-[#F9F7F3]");
+    }
+});
